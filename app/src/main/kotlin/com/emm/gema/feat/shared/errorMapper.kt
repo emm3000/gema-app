6bad950f733b530543b67e7840885ccee7e33f5e
Edgardo Muñoz <edgardo.emm20@gmail.com>
2025-06-14 10:36:40 -0500

    feat: Refactor normalizeErrorMessage, to show http exception

--- app/src/main/kotlin/com/emm/gema/feat/shared/errorMapper.kt
+++ app/src/main/kotlin/com/emm/gema/feat/shared/errorMapper.kt
@@ -1,5 +1,11 @@
 package com.emm.gema.feat.shared
 
+import retrofit2.HttpException
+
 fun normalizeErrorMessage(throwable: Throwable): String {
+    if (throwable is HttpException) return """Ocurrio un error inesperado, intenta nuevamente
+        ${throwable.code()}: ${throwable.message()}
+        ${throwable.response()?.errorBody()?.string()}
+    """.trimIndent()
     return "Ocurrio un error inesperado, intenta nuevamente\n${throwable::class.simpleName}"
 }
