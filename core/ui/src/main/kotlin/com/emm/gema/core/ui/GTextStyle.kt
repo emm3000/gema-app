699e539958f134d6d5b5209288edbe3a22e547af
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-09-11 07:01:09 -0500

    feat(ui): add GText with a closed style set

---
+++ core/ui/src/main/kotlin/com/emm/gema/core/ui/GTextStyle.kt
@@ -0,0 +1,11 @@
+package com.emm.gema.core.ui
+
+enum class GTextStyle {
+    TITLE_MEDIUM,
+    TITLE_SMALL,
+    BODY_LARGE,
+    BODY_MEDIUM,
+    BODY_SMALL,
+    LABEL_MEDIUM,
+    LABEL_SMALL,
+}
