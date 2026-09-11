5692068caae7a80ff60ed0d337d4ce11ab24d47e
Edgardo Muñoz <edgardo.emm20@gmail.com>
2026-09-11 10:52:28 -0500

    build(build-logic): add shared Kotlin helpers for the convention plugins

---
+++ build-logic/convention/src/main/kotlin/com/emm/gema/buildlogic/KotlinAndroid.kt
@@ -0,0 +1,27 @@
+package com.emm.gema.buildlogic
+
+import com.android.build.api.dsl.CommonExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension) {
+    commonExtension.compileSdk = COMPILE_SDK
+    commonExtension.defaultConfig.minSdk = MIN_SDK
+    commonExtension.compileOptions.sourceCompatibility = JAVA_VERSION
+    commonExtension.compileOptions.targetCompatibility = JAVA_VERSION
+
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions.jvmTarget.set(JVM_TARGET)
+    }
+}
+
+internal fun Project.configureAndroidUnitTestDependencies() {
+    dependencies {
+        add("testImplementation", libs.library("junit"))
+        add("testImplementation", libs.library("truth"))
+        add("testImplementation", libs.library("turbine"))
+        add("testImplementation", libs.library("kotlinx-coroutines-test"))
+    }
+}
